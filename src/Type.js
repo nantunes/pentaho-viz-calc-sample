72ceab5f0be9dcdfe56839c54cb5e24e4f838d9f
Nelson Antunes <nelson.antunes@webdetails.pt>
2016-01-19 15:52:23 +0000

    Sample

---
+++ src/Type.js
@@ -0,0 +1,38 @@
+define([
+  "pentaho/visual/base/modelFactory",
+  "pentaho/i18n!Type"
+  //"./theme/type"
+], function(visualFactory, bundle) {
+
+  "use strict";
+
+  return function(context) {
+
+    var Visual = context.get(visualFactory);
+
+    return Visual.extend({
+      meta: {
+        id: "pentaho-viz-calc-sample",
+        v2Id: "pentaho-viz-calc-sample",
+
+        view: "pentaho-viz-calc-sample/src/View",
+        styleClass: "pentaho-viz-calc-sample-style",
+        props: [
+          {
+            name: "measure",
+            required: true
+          },
+          {
+            name: "operation",
+            type: {
+              base: "string",
+              domain: ["min", "max", "avg", "sum"]
+            },
+            value: "min"
+          }
+        ]
+      }
+    })
+    .implement({meta: bundle.structured});
+  };
+});
